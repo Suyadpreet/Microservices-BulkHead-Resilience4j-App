cbcd38d97b237c1f6efdfe720a71a915f8eab345
Suyadpreet <Suyadpreet@gmail.com>
2024-04-30 12:41:43 +0530

    files added

---
+++ src/main/java/com/suyad/MicroservicesBulkHeadResilience4jAppApplication.java
@@ -0,0 +1,15 @@
+package com.suyad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBulkHeadResilience4jAppApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(MicroservicesBulkHeadResilience4jAppApplication.class, args);
+		System.out.println("SpringBoot BulkHead Resilience4j has been started Successfully.........");
+	}
+
+}
